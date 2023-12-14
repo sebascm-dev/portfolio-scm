0fa4a11803ee33f189086a265697fdedea4020ca
sebascm-dev <spanolocabo@gmail.com>
2023-12-14 19:48:57 +0100

    Add loading animation and success message to contact form

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,14 +1,17 @@
 'use client'
-import React, { useRef, RefObject, FormEvent } from 'react';
+import React, { useRef, RefObject, FormEvent, useState } from 'react';
 import emailjs from '@emailjs/browser';
 
 type Props = {};
 
 export default function Contact({ }: Props) {
+    const [sending, setSending] = useState(false);
+    const [messageSent, setMessageSent] = useState(false);
     const form: RefObject<HTMLFormElement> = useRef<HTMLFormElement>(null);
 
     const sendEmail = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
+        setSending(true); // Comienza la animación de carga
 
         if (form.current) {
             emailjs
@@ -22,9 +25,15 @@ export default function Contact({ }: Props) {
                     (result) => {
                         console.log(result.text);
                         console.log('Email sent successfully!');
+                        setSending(false); // Finaliza la animación de carga
+                        setMessageSent(true); // Establece el mensaje de enviado
+                        setTimeout(() => {
+                            setMessageSent(false); // Después de un tiempo, vuelve al estado original
+                        }, 3000); // 3000ms (3 segundos) antes de volver al estado original
                     },
                     (error) => {
                         console.log(error.text);
+                        setSending(false); // Finaliza la animación de carga si hay un error
                     }
                 );
         }
@@ -47,7 +56,12 @@ export default function Contact({ }: Props) {
                     <label className="block mb-2 text-sm font-bold">Message:</label>
                     <textarea name="message" minLength={40} required className="w-full h-auto px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:ring-2 focus:ring-[#F5B041] transition-all duration-500 focus:shadow-outline" />
 
-                    <input type="submit" value="Send" className="mail-btn w-full px-4 py-2 mt-2 mb-4 font-bold text-white bg-[#F5B041] hover:bg-yellow-700 cursor-pointer transition-all duration-500 rounded-md focus:outline-none focus:shadow-outline" />
+                    <input
+                        type="submit"
+                        value={sending ? 'Sending...' : (messageSent ? 'Message Received!' : 'Send Message')}
+                        className={`mail-btn w-full px-4 py-2 mt-2 mb-4 font-bold text-white ${messageSent ? 'bg-green-600' : 'bg-[#F5B041]'} hover:bg-yellow-700 cursor-pointer transition-all duration-500 rounded-md focus:outline-none focus:shadow-outline`}
+                        disabled={sending || messageSent}
+                    />
 
                     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12694.218710066125!2d-6.608713317031148!3d37.30568682149113!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd11efeaa0164cdb%3A0xc5c8ef10f1ac5f80!2s21720%20Rociana%20del%20Condado%2C%20Huelva!5e0!3m2!1ses!2ses!4v1702421659805!5m2!1ses!2ses" className='rounded-md w-full' height="240px" loading="lazy"></iframe>
                 </form>
