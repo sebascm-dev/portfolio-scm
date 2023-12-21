32b5c2d80800eb8dcdec829318c04d48a3aee1ce
sebascm-dev <spanolocabo@gmail.com>
2023-12-21 16:13:54 +0100

    Refactor Projects component imports

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,7 +1,8 @@
 'use client'
 import React, { useState } from 'react';
-import { motion } from 'framer-motion';
 import Image from 'next/image';
+
+import { motion } from 'framer-motion';
 
 type Props = {}
 
@@ -142,24 +143,6 @@ export default function Projects({ }: Props) {
 }
 
 export const projects = [
-    {
-        id: 2,
-        name: 'TMDB MoviesAPI',
-        status: 'Working',
-        description: 'Project guided by a video where I learn to consume an API of movies and series, and to display them on a web, showing it as if it were a catalog of movies in the style of Netflix.',
-        tags: ['#api', '#webdevelopment', '#frontend', '#backend', '#practice', '#learning'],
-        skills: ['html', 'sass', 'javascript', 'react', 'vite', 'nodejs'],
-        image: 'img/tmdbapi.png',
-        url: '',
-        is: [
-            'https://cdn-icons-png.flaticon.com/128/732/732212.png', // html
-            'https://img.icons8.com/?size=80&id=78Fr72VCwbPq&format=png', // sass
-            'https://img.icons8.com/?size=48&id=108784&format=png', // javascript
-            'https://img.icons8.com/?size=48&id=dJjTWMogzFzg&format=png', // vite
-            'https://img.icons8.com/?size=80&id=asWSSTBrDlTW&format=png', // react
-            'https://img.icons8.com/?size=48&id=hsPbhkOH4FMe&format=png' // nodejs
-        ]
-    },
     {
         id: 1,
         name: 'Web FMC - UHU',
